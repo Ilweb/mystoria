f7aba3d15dc52d000a2c90db918468b4e5f5c05e
yoannadp <yoanna_petkova@abv.bg>
2017-01-04 16:23:21 +0200

    c

---
+++ controllers/contacts.php
@@ -0,0 +1,22 @@
+<?php
+$this->showView('sticky_header');
+?>
+<div class="info_inner">
+	<h3>Contact us</h3>
+	<div><p>Lorem ipsum dollar sit amet</p></div>
+	<div class="cont_e">
+		<div>
+			<div class="under">Address</div>
+			<div class="under_info">1504 Sofia<br>
+			Tsar Osvoboditel 15, CA 94043</div>
+			<div class="under">Email</div>
+			<div class="under_info">hello@escapemystoria.com</div>
+			<div class="under">Phone number</div>
+			<div class="under_info">+359 888 11 22 33</div>
+		</div>
+		<div id="wall_5"  data-stellar-background-ratio="0.5">
+			
+		</div>
+	</div>
+	<div></div>
+</div>
