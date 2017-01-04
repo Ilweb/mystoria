8e47e5c262a963592884d70d9b9291df42d89509
yoannadp <yoanna_petkova@abv.bg>
2017-01-04 11:01:16 +0200

    FAQ.php

---
+++ html/FAQ.php
@@ -0,0 +1,59 @@
+<?php
+$this->showView('sticky_header');
+?>
+<div class="info_inner FAQ">
+	<h3>FAQ</h3>
+	<div><p>Frequently asked questions</p></div>
+	<div>
+		<div>
+		<div class="info_part">
+			<h4>Game record</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+			Nunc laoreet hendrerit dolor, et vestibulum quam
+			sodales eu. Quisque in tortor nunc. Curabitur porttitor
+			molestie sem viverra lacinia. Proin bibendum non turpis
+			non mollis. Aliquam placerat enim venenatis dolor dapibus,
+			id lobortis justo lacinia. Fusce sagittis malesuada magna. </p>
+		</div>
+		<div class="info_part">
+			<h4>Escape game success</h4>
+			<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+			Nunc laoreet hendrerit dolor, et vestibulum quam
+			sodales eu. Quisque in tortor nunc. Curabitur porttitor
+			molestie sem viverra lacinia. Proin bibendum non turpis
+			non mollis. Aliquam placerat enim venenatis dolor dapibus,
+			id lobortis justo lacinia. Fusce sagittis malesuada magna. </div>
+		</div>
+		</div>
+	<div class="book"><img src="images/FAQ/book.jpg"></div>
+		<div>
+			<div>
+				<div class="info_part">
+					<h4>Game record</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Nunc laoreet hendrerit dolor, et vestibulum quam
+					sodales eu. Quisque in tortor nunc. Curabitur porttitor
+					molestie sem viverra lacinia. Proin bibendum non turpis
+					non mollis. Aliquam placerat enim venenatis dolor dapibus,
+					id lobortis justo lacinia. Fusce sagittis malesuada magna. </p>
+				</div>
+				<div class="info_part">
+					<h4>Escape game success</h4>
+					<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Nunc laoreet hendrerit dolor, et vestibulum quam
+					sodales eu. Quisque in tortor nunc. Curabitur porttitor
+					molestie sem viverra lacinia. Proin bibendum non turpis
+					non mollis. Aliquam placerat enim venenatis dolor dapibus,
+					id lobortis justo lacinia. Fusce sagittis malesuada magna. </div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="add_explanation">
+		<div><h5>ESCAPE GAME TIME?</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet hendrerit dolor, et vestibulum quam sodales eu. Quisque in tortor nunc. Curabitur porttitor molestie sem viverra lacinia. Proin bibendum non turpis non mollis. Aliquam placerat enim venenatis dolor dapibus, id lobortis justo lacinia. Fusce sagittis malesuada magna.</p></div>
+		<div><h5>ESCAPE GAME TIME?</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet hendrerit dolor, et vestibulum quam sodales eu. Quisque in tortor nunc. Curabitur porttitor molestie sem viverra lacinia. Proin bibendum non turpis non mollis. Aliquam placerat enim venenatis dolor dapibus, id lobortis justo lacinia. Fusce sagittis malesuada magna.</p></div>
+		<div><h5>ESCAPE GAME TIME?</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet hendrerit dolor, et vestibulum quam sodales eu. Quisque in tortor nunc. Curabitur porttitor molestie sem viverra lacinia. Proin bibendum non turpis non mollis. Aliquam placerat enim venenatis dolor dapibus, id lobortis justo lacinia. Fusce sagittis malesuada magna.</p></div>
+		<div><h5>ESCAPE GAME TIME?</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet hendrerit dolor, et vestibulum quam sodales eu. Quisque in tortor nunc. Curabitur porttitor molestie sem viverra lacinia. Proin bibendum non turpis non mollis. Aliquam placerat enim venenatis dolor dapibus, id lobortis justo lacinia. Fusce sagittis malesuada magna.</p></div>
+	</div>
+	<div onclick="window.location.href = '<?php echo LOCALE_URL; ?>'" class="choose second"><div>Learn more</div></div>
+</div>
