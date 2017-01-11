c5e0bb5cebac907684a25bde08a03f60fb6d34b4
yoannadp <yoanna_petkova@abv.bg>
2017-01-11 16:58:54 +0200

    admin radio btn

--- html/adminNav.php
+++ html/adminNav.php
@@ -1,16 +1,13 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Mystoria</a>
+      <a class="navbar-brand" href="<?php echo LOCALE_URL; ?>">Mystoria</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="index.php?content=articles">Reservation</a></li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Articles <span class="caret"></span></a>
-    <ul class="dropdown-menu">
-      <li><a href="index.php?content=articles&action=edit&id=<?php echo $article->id; ?>" ><?php echo $lang['Edit']; ?></a></li>
-      <li><a  href="index.php?content=articles&action=edit"><?php echo $lang['New article']; ?></a></li>
-    </ul>
-      <li><a href="index.php?content=pages">Page </a></li>
+      <li ><a href="index.php?content=reservations">Резервации</a></li>      
+      <li><a href="index.php?content=articles&action=edit">Нова статия</a></li>
+    
+      <li><a href="index.php?content=pages">Страници </a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="#"><span class="fa fa-lock" aria-hidden="true"></span> </a></li>
