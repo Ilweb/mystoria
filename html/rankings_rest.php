61b27c2b6bf0fbd0179f5c545e4f05fbdf942144
mysticizm <ivan.lisnik@ilweb.eu>
2017-01-11 16:04:55 +0200

    Rankings pagination works

--- html/rankings_rest.php
+++ html/rankings_rest.php
@@ -46,20 +46,20 @@ if (isset($pages) && ($pages > 1))
 	if ($page > 1)
 	{
 		?>
-		<li><a href="#" data-page="<?php echo $page - 1; ?>" data-toggle="tooltip" title="Previous page"><span class="fa fa-chevron-left"></span></a></li>
+		<li><a data-page="<?php echo $page - 1; ?>" data-toggle="tooltip" title="Previous page"><span class="fa fa-chevron-left"></span></a></li>
 		<?php
 	}
 	for ($i = 1; $i <= $pages; $i++)
 	{
 		if (($i == 1) || ($i == $pages) || (($i > $page - 3) && ($i < $page + 3)))
 		{
-			echo '<li'.($page == $i ? ' class="active"' : '').'><a href="#" data-page="'.$i.'">'.$i.'</a></li>';
+			echo '<li'.($page == $i ? ' class="active"' : '').'><a data-page="'.$i.'">'.$i.'</a></li>';
 		}
 	}
 	if ($page < $pages)
 	{
 		?>
-		<li><a href="#" data-page="<?php echo $page + 1; ?>" data-toggle="tooltip" title="Next page"><span class="fa fa-chevron-right"></span></a></li>
+		<li><a data-page="<?php echo $page + 1; ?>" data-toggle="tooltip" title="Next page"><span class="fa fa-chevron-right"></span></a></li>
 		<?php
 	}
 	?>
