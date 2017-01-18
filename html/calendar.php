f5ad447a9596226e8c16e19b607f8fbf52003741
yoannadp <yoanna_petkova@abv.bg>
2017-01-18 10:01:33 +0200

    jQuery

--- html/calendar.php
+++ html/calendar.php
@@ -35,3 +35,22 @@ for ($i = 0; $i < 7; $i++)
 	<?php
 }
 ?>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+    if ( $(window).width() > 480) {
+    	$('.dates1 >li').click(function(){
+        	$(this).children('hour').stop(true).show().css('display:block');
+};
+
+};
+});
+</script>
+<script type="text/javascript">	
+jQuery(".dates1  ").click(function()
+
+	{
+		jQuery(" li.hour ").toggle('slow').css("position","absolute").css('cursor','pointer' )
+	});
+
+</script>
